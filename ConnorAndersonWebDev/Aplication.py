e087a2ac740c8d8f3fedb883cf38a0e4815f1c1a
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-09-12 14:03:50 -0600

    Add files via upload

---
+++ ConnorAndersonWebDev/Aplication.py
@@ -0,0 +1,8 @@
+print("Hello, possible pirate! What's the password?")
+greeting = input()
+
+if (greeting == "Arrr!"):
+
+	print("Go away, pirate.")
+
+else: print("Greetings, hater of pirates!")
---
+++ ConnorAndersonWebDev/Code.js
@@ -0,0 +1,11 @@
+var x = 0;
+var R_Per_Second = 1;
+var T = 0;
+function ClickCounter(Amount) {
+    if(t == 0){
+        t = Date.UTC
+    }
+
+    x = x + 1;
+    document.getElementById("count").innerHTML = x;
+}
