decf35d5d982ff173bdc30610b4b639119444c99
Connor Anderson <andersonconnor09@gmail.com>
2023-09-22 12:21:12 -0600

    click update

--- clicker/ClickerGame.js
+++ clicker/ClickerGame.js
@@ -1,35 +1,90 @@
-var x = 0;
-var R_Per_Second = 1;
+var Paper = 0;
+var P_Per_Second = 10000000;
 var t = new Date();
 var last_t = new Date();
+var num = 0;
+var num_names = ["", " Thousand", " Million", " Billion", "Trillion", " Quadrillion", " Quintillion", " Septillion"]
+var Upgrades = [
+    {price:10, name: "Auto Clicker", max:100000, amount:0, description:"Automaticly farms for paper"},
+    {price: 100, name: "Ultra Clicker", max:100000, amount: 0, description:"This is more advanced and crazy version of the autoclicker that you should totaly buy ;)"}
+]
+var bulkPurchase = 1;
 function ClickCounter(Amount) {
 
-    x = x + Amount;
-    document.getElementById("count").innerHTML = x;
+    Paper = Paper + Amount;
+    UpdateCounter()
 }
 
-function subAuto(Amount) {
-    R_Per_Second -= Amount;
-    document.getElementById("autoAmount").innerHTML = R_Per_Second;
+function UpdateCounter() {
+    document.getElementById("count").innerHTML = shrinkNumber(Paper) + " Paper";
+    document.getElementById("autoAmount").innerHTML = bulkPurchase;
 }
 
-function addAuto(Amount) {
-    R_Per_Second += Amount;
-    document.getElementById("autoAmount").innerHTML = R_Per_Second;
+function subAmount(Amount) {
+    bulkPurchase = bulkPurchase / Amount;
+    document.getElementById("autoAmount").innerHTML = bulkPurchase;
+}
+
+function addAmount(Amount) {
+    bulkPurchase = bulkPurchase * Amount;
+    document.getElementById("autoAmount").innerHTML = bulkPurchase;
 }
 
 function AddClicks () {
-    x = x + (R_Per_Second*((last_t - t)));
+    Paper = Paper + (P_Per_Second*((t - last_t)/1000));
 }
 
 setInterval(GameTick,1);
 
 function GameTick() {
 
-    t = new Date().getUTCSeconds();
+    P_Per_Second = Upgrades[0].amount + Upgrades[1].amount * 100;
+    t = new Date();
     AddClicks();
     last_t = t;
-    document.getElementById("count").innerHTML = x;
+    UpdateCounter()
+    checkUpgrades()
+}
+
+function shrinkNumber(number) {
+    var num_idx = 0;
+    num = number;
+    while(num > 1000){
+        num = num / 1000;
+        num_idx++;
+    }
+
+    num = Math.floor(num * 100)/100 + num_names[num_idx];
+    if(num_idx == 0){
+        num = Math.floor(num)
+    }   
+    
+    return(num)
+}
+
+function Upgrade(id, elementId) { 
+
+    if(Paper >= Upgrades[id].price * bulkPurchase){
+        Paper = Paper - Upgrades[id].price * bulkPurchase;
+        Upgrades[id].amount += 1 * bulkPurchase;
+        document.getElementById(elementId).getElementsByClassName("price")[0].innerHTML = Upgrades[id].name + ": " + Upgrades[id].price * bulkPurchase;
+        document.getElementById(elementId).getElementsByClassName("desc")[0].innerHTML = Upgrades[id].description;
+
+    }
 
 }
 
+// function checkUpgrades() {
+//     var i = 0;
+//     while(i < document.getElementsByClassName("Upgrade").length){
+//         if(document.getElementsByClassName("Upgrade")[i].getElementsByClassName("price") <= Paper + P_Per_Second * 100 || document.getElementsByClassName("Upgrade")[i].getElementsByClassName("price") <= Paper)
+//         {
+//             document.getElementsByClassName("Upgrade")[i].getElementsByClassName("price").innerHTML = Upgrades[i].name + ": " + Upgrades[i].price;
+//             document.getElementsByClassName("Upgrade")[i].getElementsByClassName("price").innerHTML = Upgrades[i].description;
+//         } else {
+//             document.getElementsByClassName("Upgrade")[i].getElementsByClassName("price").innerHTML = "";
+//         }
+//         i += 1;
+//     }
+
+// }
