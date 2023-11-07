edef2c5261cc2b9a272f3f38ae47b980c40e0589
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-11-07 14:40:16 -0700

    Add files via upload

---
+++ WebDev/variables/Variables.js
@@ -0,0 +1,39 @@
+const firstName = "Connor"
+const middleName = "James"
+lastName = "Anderson Barrera"
+
+age = 14
+grade = 9
+eyes = 2
+
+rich = false
+wantsToBeRich = true
+hasTable = true
+
+friends = ["Landon", "Ryan", "Bruce", "ect"]
+foods = ["burger", "sandwich", "oatmeal"]
+tables = ["round", "tall", "foldable", "desk"]
+
+Connor = { age: 14, firstName : "Connor" }
+flavors = { Chocolate: true, Vanilla: true, Crubleberry: false}
+Kyle = {name: "kyle", age: 52, happy: true}
+
+document.getElementById("1").innerHTML = firstName;
+document.getElementById("2").innerHTML = middleName;
+document.getElementById("3").innerHTML = lastName;
+
+document.getElementById("4").innerHTML = age;
+document.getElementById("5").innerHTML = grade;
+document.getElementById("6").innerHTML = eyes;
+
+document.getElementById("7").innerHTML = rich;
+document.getElementById("8").innerHTML = wantsToBeRich;
+document.getElementById("9").innerHTML = hasTable;
+
+document.getElementById("10").innerHTML = friends;
+document.getElementById("11").innerHTML = foods;
+document.getElementById("12").innerHTML = tables;
+
+document.getElementById("13").innerHTML = Connor;
+document.getElementById("14").innerHTML = flavors;
+document.getElementById("15").innerHTML = Kyle;
