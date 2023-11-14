eed1c58557472f7958d1a25408a2940878b954d3
Connor Anderson <143214411+StonksPerson@users.noreply.github.com>
2023-11-14 14:33:31 -0700

    Add files via upload

--- WebDev/Functions/func.js
+++ WebDev/Functions/func.js
@@ -1,62 +1,31 @@
-function tellFortune(childNum, partnerName, location, job){
-    return(`You will be a ${job} in ${location}, and married to ${partnerName} with ${childNum} kids`)
+function tellFortune(){
+    document.getElementById("fortune").innerHTML = `You will be married to ${document.getElementById("name").value} with ${document.getElementById("children").value} kids`;
 }
 
-function calculateDogAge(dogAge){
-    return(`Your dog is ${dogAge * 7} years old in dog years!`)
+function calculateDogAge(){
+    document.getElementById('age').innerHTML = `Your dog is ${document.getElementById("dog-age").value * 7} years old in dog years!`;
 }
 
-function calculateSupply(age, perDay){
-    return(`You will need ${perDay * age * 365} to last you until the ripe old age of ${age}`)
+function calculateSupply(){
+    amountPerDay = document.getElementById("amount-per-day").value;
+    age = document.getElementById("current-age").value;
+    totalAmount = (90 - age) * amountPerDay * 365.25;
+
+    document.getElementById('supply').innerHTML = `You will need ${totalAmount} to last you until the ripe old age of ${90}`;
 }
 
-function calcCircumfrence(radius){
-    return(`The circumfrence is ${Math.PI * radius}`)
+function calculateCircumference(){
+    document.getElementById("circumference").innerHTML = `The circumfrence is ${Math.PI * document.getElementById("radius").value}`
 }
 
-function calcArea(radius){
-    return(`The area is ${(2 * Math.PI) * (Math.pow(radius, 2))}`)
+function calcArea(){
+    document.getElementById("area").innerHTML = `The area is ${(2 * Math.PI) * (Math.pow(document.getElementById("radius2").value, 2))}`;
 }
 
-function celsiusToFahrenheit(degrees){
-    return(`${degrees} degrees Celsius is ${(degrees * (9/5)) + 32 } degrees farenheit`)
+function celsiusToFahrenheit(){
+    document.getElementById("fahrenheit").innerHTML = `${document.getElementById("celcius").value} degrees Celsius is ${(document.getElementById("celcius").value * (9/5)) + 32 } degrees farenheit`;
 }
 
-function fahrenheitToCelsius(degrees){
-    return(`${degrees} degrees Farenheit is ${(degrees - 32) * 5/9} degrees celsius`)
+function fahrenheitToCelsius(){
+    document.getElementById("celsius2").innerHTML = `${document.getElementById("fahrenheit2").value} degrees Farenheit is ${(document.getElementById("fahrenheit2").value - 32) * (5 / 9)} degrees celsius`;
 }
-
-tellFortune(5, "januel", "Portugul", "Work")
-tellFortune(50, "help", "america", "parent")
-tellFortune(1, "micelium", "brazil", "man")
-tellFortune(7, "markle", "Target", "Target")
-
-calculateDogAge(5)
-calculateDogAge(3)
-calculateDogAge(190)
-calculateDogAge(6)
-
-calculateSupply(2,5)
-calculateSupply(6,40)
-calculateSupply(1,10)
-calculateSupply(50,50)
-
-calcCircumfrence(5)
-calcCircumfrence(6)
-calcCircumfrence(2)
-calcCircumfrence(1)
-
-calcArea(4)
-calcArea(3)
-calcArea(5)
-calcArea(6)
-
-celsiusToFahrenheit(2)
-celsiusToFahrenheit(6)
-celsiusToFahrenheit(36)
-celsiusToFahrenheit(2)
-
-fahrenheitToCelsius(79)
-fahrenheitToCelsius(89)
-fahrenheitToCelsius(59)
-fahrenheitToCelsius(378)
